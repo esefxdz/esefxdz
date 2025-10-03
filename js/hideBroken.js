789afa0d3ab8217855f7aae1be3e70a9cf6b12eb
esefxdz <esefqz39@gmail.com>
2025-10-03 11:59:35 +0300

    bropls

--- js/hideBroken.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('img').forEach(img => {
-    img.onerror = () => {
-      img.style.display = 'none';
-    };
-  });
-});
